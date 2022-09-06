d746e03a7ec184b6ee153b47778140753f68e86d
Iman Khalid <imankhalid@Imans-MacBook-Air.local>
2022-09-06 19:11:06 +0200

    update

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -7,80 +7,35 @@ import { Heading2, Heading3, Heading4 } from "../styleGuid/components/text";
 import Image from 'next/image';
 
 export default function Projects({ projects }) {
-  console.log("aaa", projects);
-  return (
     <section className={styles.section2} id="proj">
-       <div className={styles.row}>
-        <div>
-          <Heading2 className={styles.title}>
-            Projects
-          </Heading2>
-        </div>
-      </div>
-
-      {projects?.map((project, i) => {
-
-        // let projectClasses = [styles.row];
-
-        // if (i % 2) {
-        //   projectClasses.push(styles.leftCard)
-        // } else {
-        //   projectClasses.push(styles.rightCard)
-        // }
-
-
-        // projectClasses.join(' ');
-        
-        return (
-        <div key={i}>
-          
-      <div className={styles.aw}>
         <div className={styles.row}>
-        <div className={styles.card1}>
-          <Heading3>Project-1</Heading3> 
-    
-    <div className={styles.pic}>  <Image
-        src={project.projpic.url}
-        width={500}
-        height={690}
-      /></div> 
-        <div className={styles.infos}>
-                    <Heading3 className={styles.projectcard__title}>
-                      {project.title}
-                    </Heading3>
-                    <Heading4 className={styles.projectcard__description}>
-                      {project.description}
-                    </Heading4>
-
-                    <a href="" className={styles.projectcard__link}>
-                      {project.date}
-                    </a>
-                  </div>
-         </div>
-    {/* <div className={styles.card2}>
-      <Image
-        src="/vercel.svg"
-        width="300px"
-        height="200px"
-      />
-      <h2>Card here </h2>
-      <p> lorem ipsum here now to get here </p>
-    </div>
-
-    <div className={styles.card3}>
-      <Image
-        src="/vercel.svg"
-        width="300px"
-        height="200px"
-      />
-      <h2>Card here </h2>
-      <p> lorem ipsum here now to get here </p>
-    </div> */}
+            <div>
+                <Heading2 className={styles.title}>
+                    Projects
+                </Heading2>
         </div>
-       
-      </div> 
         </div>
-        )})}
+
+        {projects.map((project,i)=>{
+            <div key={i}>
+                <div className={styles.aw}>
+                    <div className={styles.row}>
+                        <div className={styles.card1}>
+                              <div className={styles.pic}>
+                                <Image
+                                    src={project.projpic.url}
+                                    width={500}
+                                    height={690}
+                                /></div>
+                              <div className={styles.info}>
+                                    <Heading3 className={styles.projectcard__title}>{project.title}</Heading3>
+                                    <Heading4 className={styles.projectcard__description}>{project.description}</Heading4>
+                                    <a href="" className={styles.projectcard__link}>{project.data}</a>
+                              </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        })}
     </section>
-  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import client from "../apolloClient";
 import { Header } from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 // import Projects from "../components/About/About";
-// import Projects from "../components/Projects/Projects"
+import Projects from "../components/Projects/Projects"
 import About from "../components/About/About";
 
 export default function Home({ aboutMes, achievements, projects }) {
@@ -14,7 +14,7 @@ export default function Home({ aboutMes, achievements, projects }) {
       <title>Iman Al Wahaibi</title>
       <Header aboutMes={aboutMes} />
 
-      {/* <Projects projects={projects} />*/}
+      <Projects projects={projects} />
      <About aboutMes={aboutMes} /> 
       
       <Footer />
